4cacf710032ba4b3f82866e0268fa94c7431f35c
deveshjeewani <deveshjeewani@gmail.com>
2023-08-31 10:04:33 +0530

    Initial commit

---
+++ src/main/java/com/devesh/demo/Greeting.java
@@ -0,0 +1,21 @@
+package com.devesh.demo;
+
+public class Greeting {
+
+    private Long id;
+    private String content;
+
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
